78423d6c9551396396923752d5488e9399cc6103
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2022-06-22 16:17:12 +0300

    Prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -32,15 +32,22 @@ app.get("/api/config/paypal", (req, res) =>
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-app.use(express.static(path.join(__dirname, "/frontend/build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "frontend/build/index.html"))
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+
+console.log(
+  "Testing",
+  path.resolve(__dirname, "frontend", "build", "index.html")
 );
-
-app.get("/", (req, res) => {
-  res.send("API is running....");
-});
 
 app.use(notFound);
 app.use(errorHandler);
